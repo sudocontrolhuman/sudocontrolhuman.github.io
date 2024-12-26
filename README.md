e5500026332715d3239abb957b9e1b3020b88e6c
sudocontrolhuman <yaroslav.joseph.bond@gmail.com>
2024-12-26 12:07:57 -0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# anythingicanthinkof
+# anythingicanthinkof
---
+++ funny/test/index.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    var name = prompt("you are heavy");
+    var lengthOfName = ("your input:  ")+name
+
+    document.getElementById('output').innerHTML = lengthOfName;
+};
+alert("YOU ARE SUPER FAT. hehehehehe");
+
+function doSomething() {
+    alert('you got 94769438639876 dollars!!');
+  }
+
+  
